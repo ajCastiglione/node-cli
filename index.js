7f877de830035a0cd60b30ad65b6ff11fafce057
Aj Castiglione <antoniocastiglione10@gmail.com>
2018-11-25 22:47:32 -0500

    added input for directory, catch for ensuring dir is empty, passing dir to functions generating code / downloading dependencies

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 #!/usr/bin/env node
 
 const program = require("commander");
+const fs = require("fs");
 const { createFiles, installModules } = require("./actions");
+const { testFunc } = require("./test");
 
 program
-  .version("1.0.0")
+  .version("1.0.1")
   .description("CLI to generate basic node / express app");
 
 program
-  .command("express")
+  .command("exp")
+  .arguments("<project-directory>")
   .alias("e")
   .description("Generate node / express application with JWT")
-  .action(() => {
+  .action(dir => {
+    let files = fs.readdirSync(dir);
+    if (files.length > 0) {
+      console.log(
+        "The chosen directory must be empty. To proceed, please choose another directory or remove the existing files."
+      );
+      process.exit(1);
+    }
     console.info("Generating express app...");
-    createFiles();
+    createFiles(dir);
     console.info("Successfully created express files");
     console.info("Downloading dependencies...");
-    installModules();
+    installModules(dir);
   });
 
 program.parse(process.argv);
